f6197a7a187b11533bfbcb40c5285161452d8cf3
Gayatri Pisya <gayatripisya@gmail.com>
2024-09-20 13:04:04 +0530

    updated

--- ReportAndAnalysisBackEnd/src/main/java/com/st/project/MonthlyUsersRepository.java
+++ ReportAndAnalysisBackEnd/src/main/java/com/st/project/MonthlyUsersRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MonthlyUsersRepository extends JpaRepository<MonthlyUser, Long> {
+public interface MonthlyUsersRepository extends JpaRepository<MonthlyUser, String> {
 }
