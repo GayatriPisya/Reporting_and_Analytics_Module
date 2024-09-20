591fd71a300a1c77e96a6af5eae89111def5e6a1
Gayatri Pisya <gayatripisya@gmail.com>
2024-09-20 10:02:07 +0530

    updated

--- ReportAndAnalysisBackEnd/src/main/java/com/st/project/MonthlyUser.java
+++ ReportAndAnalysisBackEnd/src/main/java/com/st/project/MonthlyUser.java
@@ -1,19 +1,34 @@
 package com.st.project;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "monthly_users")
 public class MonthlyUser {
+
+    @Id
     private String month;
     private int count;
-	public String getMonth() {
-		return month;
-	}
-	public void setMonth(String month) {
-		this.month = month;
-	}
-	public int getCount() {
-		return count;
-	}
-	public void setCount(int count) {
-		this.count = count;
-	}
 
+    // No-argument constructor (required by JPA)
+    public MonthlyUser() {}
+
+    // Getters and setters
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
