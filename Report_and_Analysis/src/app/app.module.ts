9415498c1a3fe63062edce55a001e3e035360f98
Gayatri <157793060+GayatriPisya@users.noreply.github.com>
2024-09-21 21:21:32 +0530

    Update app.module.ts

--- Report_and_Analysis/src/app/app.module.ts
+++ Report_and_Analysis/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { HighchartsChartModule } from 'highcharts-angular';
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { RealtimeChartComponent } from './realtime-chart/realtime-chart.component';
-import { HistoricalChartComponent } from './historical-chart/historical-chart.component';
+import { HighchartsChartModule } from 'highcharts-angular';
+
+import { AppComponent } from './app.component';
 import { ReportComponent } from './report/report.component';
+import { ReportingService } from './reporting.service';
 
 @NgModule({
-  declarations: [AppComponent, RealtimeChartComponent, HistoricalChartComponent, ReportComponent],
-  imports: [BrowserModule, HighchartsChartModule, HttpClientModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    ReportComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    HighchartsChartModule  // Add Highcharts module here
+  ],
+  providers: [ReportingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
