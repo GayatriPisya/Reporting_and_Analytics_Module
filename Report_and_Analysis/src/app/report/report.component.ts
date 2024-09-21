0bc5744660e1cbb5ff2801886f6ec056dff75ca9
Gayatri <157793060+GayatriPisya@users.noreply.github.com>
2024-09-21 21:20:54 +0530

    Update report.component.ts

--- Report_and_Analysis/src/app/report/report.component.ts
+++ Report_and_Analysis/src/app/report/report.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReportingService, MonthlyUser } from '../reporting.service';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrl: './report.component.css'
+  styleUrls: ['./report.component.css']
 })
-export class ReportComponent {
+export class ReportComponent implements OnInit {
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {};
+  monthlyUsers: MonthlyUser[] = [];
+  errorMessage: string = '';
 
+  constructor(private reportingService: ReportingService) {}
+
+  ngOnInit(): void {
+    this.fetchMonthlyUsers();
+  }
+
+  fetchMonthlyUsers() {
+    this.reportingService.getMonthlyUsers().subscribe(
+      (data: MonthlyUser[]) => {
+        this.monthlyUsers = data;
+        this.initializeChart();
+      },
+      (error) => {
+        this.errorMessage = error;
+        console.error('There was an error!', error);
+      }
+    );
+  }
+
+  initializeChart() {
+    const categories = this.monthlyUsers.map(user => user.month);
+    const data = this.monthlyUsers.map(user => user.count);
+
+    this.chartOptions = {
+      chart: { type: 'column' },
+      title: { text: 'Monthly User Growth' },
+      xAxis: { categories: categories, title: { text: 'Month' } },
+      yAxis: { min: 0, title: { text: 'Users' } },
+      series: [{ name: 'Users', type: 'column', data: data }]
+    };
+  }
 }
