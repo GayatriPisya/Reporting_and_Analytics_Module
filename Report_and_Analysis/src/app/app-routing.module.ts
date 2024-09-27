e3a9b91a0c73be8ef27bd0a5fcaf263dd3aca7e0
Gayatri <157793060+GayatriPisya@users.noreply.github.com>
2024-09-27 11:54:44 +0530

    Update app-routing.module.ts

--- Report_and_Analysis/src/app/app-routing.module.ts
+++ Report_and_Analysis/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
+// src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ReportComponent } from './report/report.component';
+import { HistoricalChartComponent } from './historical-chart/historical-chart.component';
 
 const routes: Routes = [
-  { path: 'report', component: ReportComponent }
+  { path: 'report', component: ReportComponent },
+  { path: 'historical-chart', component: HistoricalChartComponent }
 ];
 
 @NgModule({
