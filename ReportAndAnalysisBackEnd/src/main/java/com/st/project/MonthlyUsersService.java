82f004a199553f9acb2f0ae848dd636bd07ea938
Gayatri <157793060+GayatriPisya@users.noreply.github.com>
2024-10-31 15:49:13 +0530

    Update MonthlyUsersService.java

--- ReportAndAnalysisBackEnd/src/main/java/com/st/project/MonthlyUsersService.java
+++ ReportAndAnalysisBackEnd/src/main/java/com/st/project/MonthlyUsersService.java
@@ -2,15 +2,15 @@ package com.st.project;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 public class MonthlyUsersService {
-
     @Autowired
-    private MonthlyUsersRepository repository;
+    private MonthlyUserRepository monthlyUserRepository;
 
-    public List<MonthlyUser> getAllMonthlyUsers() {
-        return repository.findAll();
+    public List<MonthlyUser> fetchUsersByYear(int year) {
+        return monthlyUserRepository.findAllByYear(year);
     }
 }
