5312ff9f2a30bbe15094544ab90bfa3cc87c597b
Gayatri <157793060+GayatriPisya@users.noreply.github.com>
2024-11-15 18:07:30 +0530

    Update MonthlyUserController.java

--- ReportAndAnalysisBackEnd/src/main/java/com/st/project/MonthlyUserController.java
+++ ReportAndAnalysisBackEnd/src/main/java/com/st/project/MonthlyUserController.java
@@ -1,16 +1,18 @@
 package com.st.project;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:4200")
 public class MonthlyUserController {
+
     @Autowired
     private MonthlyUsersService monthlyUserService;
 
